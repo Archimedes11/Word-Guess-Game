9635a2bb0ae8fbb68c54e93385f44663a2c93c0d
Adam Wolff <adamkwolff@gmail.com>
2020-04-23 19:26:05 -0500

    got most of the first and second part working. now i will focus my attention on the pictures, the songs, and ther title of the song.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Declaring all of my global variables
-var letterChoicesWaylon = ['w', 'a', 'y', 'l', 'o', 'n']
+var waylon = ['w', 'a', 'y', 'l', 'o', 'n']
 var willie = ['w', 'i', 'l', 'l', 'i', 'e']
 var displayArray = ['_', '_', '_', '_', '_', '_']
 var currentGuess = '';
@@ -7,9 +7,14 @@ var previousGuess = [];
 var wins = 0;
 var remainingGuesses = 12;
 var remainingGuessesNum = 12;
-var flag = false;
 var count = 0;
 
+document.getElementById("remainingGuesses-amount").innerHTML = remainingGuesses; //shows the amounts of guesses remaining.
+document.getElementById("win-amount").innerHTML = wins; //shows how many wins.
+document.getElementById("previousGuess-array").innerHTML = previousGuess; //shows their previous guess.
+var displayArrayString = displayArray.join(' ').trim() //Turns the array into a string for aestethics.
+document.getElementById("blanks").innerHTML = displayArrayString; // shows how many letters they have right and in what order.
+document.getElementById("picture").innerHTML = '<assets/images/Clint.jpg" alt="hangman"></img>';
 
 
 //Gets a keystroke from the user and saves in in "currentGuess"
@@ -49,7 +54,7 @@ document.onkeyup = function (event) {
 
         //checks to see if the user has won.
         if ((displayArray[0] == 'W') && (displayArray[1] == 'I') && (displayArray[2] == 'L') && (displayArray[3] == 'L') && (displayArray[4] == 'I') && (displayArray[5] == 'E')) {
-            wins=1;
+            wins = 1;
 
         }
 
@@ -81,8 +86,12 @@ document.onkeyup = function (event) {
         var displayArrayString = displayArray.join(' ').trim() //Turns the array into a string for aestethics.
         document.getElementById("blanks").innerHTML = displayArrayString; // shows how many letters they have right and in what order.
 
+        if (wins == 1) {
+            remainingGuesses = 0;
+        }
+
         if ((remainingGuesses == 0) && (wins == 0)) {
-            letterChoicesWaylon = ['w', 'a', 'y', 'l', 'o', 'n']
+            waylon = ['w', 'a', 'y', 'l', 'o', 'n']
             willie = ['w', 'i', 'l', 'l', 'i', 'e']
             displayArray = ['_', '_', '_', '_', '_', '_']
             currentGuess = '';
@@ -90,10 +99,121 @@ document.onkeyup = function (event) {
             wins = 0;
             remainingGuesses = 12;
             remainingGuessesNum = 12;
-            flag = false;
             count = 0;
         }
         return false;//This stops the god-awful key event from looping and lets my loop do the work.
     }
-};
 
+    //// second game
+
+
+    if (wins == 1) {
+        waylon = ['w', 'a', 'y', 'l', 'o', 'n']
+        willie = ['w', 'i', 'l', 'l', 'i', 'e']
+        displayArray = ['_', '_', '_', '_', '_', '_']
+        currentGuess = '';
+        previousGuess = [];
+        remainingGuesses = 13;
+        remainingGuessesNum = 13;
+        count = 0;
+
+        document.getElementById("remainingGuesses-amount").innerHTML = remainingGuesses; //shows the amounts of guesses remaining.
+        document.getElementById("win-amount").innerHTML = wins; //shows how many wins.
+        document.getElementById("previousGuess-array").innerHTML = previousGuess; //shows their previous guess.
+        var displayArrayString = displayArray.join(' ').trim() //Turns the array into a string for aestethics.
+        document.getElementById("blanks").innerHTML = displayArrayString; // shows how many letters they have right and in what order.
+
+
+
+
+
+
+
+
+
+
+        //Gets a keystroke from the user and saves in in "currentGuess"
+        document.onkeyup = function (event) {
+            var currentGuess = event.key;
+
+
+            for (i = 0; i < remainingGuesses; remainingGuesses--) {
+                console.log(remainingGuesses);
+
+
+                // decides if a correct letter was chosen
+                if (currentGuess == waylon[0]) {
+                    displayArray[0] = 'W';
+                };
+
+                if (currentGuess == waylon[1]) {
+                    displayArray[1] = 'A';
+                };
+
+                if (currentGuess == waylon[2]) {
+                    displayArray[2] = 'Y';
+                };
+
+                if (currentGuess == waylon[3]) {
+                    displayArray[3] = 'L';
+                };
+
+                if (currentGuess == waylon[4]) {
+                    displayArray[4] = 'O';
+                };
+
+                if (currentGuess == waylon[5]) {
+                    displayArray[5] = 'N';
+                };
+
+                //checks to see if the user has won.
+                if ((displayArray[0] == 'W') && (displayArray[1] == 'A') && (displayArray[2] == 'Y') && (displayArray[3] == 'L') && (displayArray[4] == 'O') && (displayArray[5] == 'N')) {
+                    wins = 2;
+
+                }
+
+                var doesIncludeCounter = 0;
+                for (i = 0; i < previousGuess.length; i++) {
+                    if (previousGuess[i] == (currentGuess)) {
+                        doesIncludeCounter++;
+                    }
+                }
+                if (doesIncludeCounter == 0) {
+                    previousGuess[count] = currentGuess; // adds the current guess to the specific index on previousGuess
+                    count++; //This counter is being used to make sure the user's guess goes in the correct index of "previous guesses"    
+                }
+                else {
+                    remainingGuesses++;
+                }
+
+
+
+
+
+
+
+                remainingGuesses--;
+
+                document.getElementById("remainingGuesses-amount").innerHTML = remainingGuesses; //shows the amounts of guesses remaining.
+                document.getElementById("win-amount").innerHTML = wins; //shows how many wins.
+                document.getElementById("previousGuess-array").innerHTML = previousGuess; //shows their previous guess.
+                var displayArrayString = displayArray.join(' ').trim() //Turns the array into a string for aestethics.
+                document.getElementById("blanks").innerHTML = displayArrayString; // shows how many letters they have right and in what order.
+
+                if ((remainingGuesses == 0) && (wins == 0)) {
+                    waylon = ['w', 'a', 'y', 'l', 'o', 'n']
+                    willie = ['w', 'i', 'l', 'l', 'i', 'e']
+                    displayArray = ['_', '_', '_', '_', '_', '_']
+                    currentGuess = '';
+                    previousGuess = [];
+                    wins = 0;
+                    remainingGuesses = 12;
+                    remainingGuessesNum = 12;
+                    count = 0;
+                }
+                return false;//This stops the god-awful key event from looping and lets my loop do the work.
+            }
+        };
+
+    }
+}
